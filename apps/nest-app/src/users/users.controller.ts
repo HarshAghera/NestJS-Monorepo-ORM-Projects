b2ad8629e4a04f728a0393fde872a2163caeab94
Harsh Patel <harsh@isummation.com>
2024-03-26 09:48:26 +0530

    Added Logger.

--- apps/nest-app/src/users/users.controller.ts
+++ apps/nest-app/src/users/users.controller.ts
@@ -10,13 +10,14 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-
+import { logger } from '../middlewares/logger.middlware';
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
+    logger(createUserDto);
     this.usersService.create(createUserDto);
     return createUserDto;
   }
@@ -28,16 +29,19 @@ export class UsersController {
 
   @Get(':id')
   findOne(@Param('id') id: string): CreateUserDto {
+    logger(id);
     return this.usersService.findOne(+id);
   }
 
   @Patch(':id')
   update(@Param('id') id: number, @Body() updateUserDto: UpdateUserDto) {
+    logger({id,updateUserDto});
     return this.usersService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
+    logger(id);
     return this.usersService.remove(+id);
   }
 }
