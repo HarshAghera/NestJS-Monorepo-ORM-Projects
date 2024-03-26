1e1462798f0f6db762a983a6c1a772c4db666f20
Harsh Patel <harsh@isummation.com>
2024-03-26 09:48:35 +0530

    Added Logger.

---
+++ apps/nest-app/src/middlewares/logger.middlware.ts
@@ -0,0 +1,16 @@
+import { createLogger } from 'winston';
+const DailyRotateFile = require('winston-daily-rotate-file');
+
+export function logger(req) {
+  const logger = createLogger({
+    level: 'info',
+    transports: [
+      new DailyRotateFile({
+        filename: 'nest_request.log',
+        maxFiles: '2d',
+        dirname: 'D:/projects/apps/nest-app/src/logs',
+      }),
+    ],
+  });
+  logger.info({ Request: req });
+}
