1f6cfc6d23870f93ae88022085a4713c36c83979
Harsh Patel <harsh@isummation.com>
2024-04-13 09:02:43 +0530

    Initial Commit.

---
+++ StandardProject/db-project-type-orm/src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { DatabaseConfigService } from './database/database-config.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useClass: DatabaseConfigService,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService, ConfigService],
+})
+export class AppModule {}
---
+++ StandardProject/db-project-type-orm/src/database/database-config.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+
+@Injectable()
+export class DatabaseConfigService implements TypeOrmOptionsFactory {
+  constructor(private configs: ConfigService) {}
+  createTypeOrmOptions(): TypeOrmModuleOptions | Promise<TypeOrmModuleOptions> {
+    return {
+      type: 'mysql',
+      host: this.configs.get('MY_SQL_HOST'),
+      port: this.configs.get('MY_SQL_PORT'),
+      username: this.configs.get('MY_SQL_USERNAME'),
+      password: this.configs.get('MY_SQL_PASSWORD'),
+      database: this.configs.get('MY_SQL_DATABASE'),
+      synchronize: false,
+      autoLoadEntities: true,
+    };
+  }
+}
